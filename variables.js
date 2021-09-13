84e025f9106fa9e8fbf193cff74069888f466721
surajm-333 <surajmulchandani37@gmail.com>
2021-09-13 17:21:56 +0530

    Learnt about variables and datatypes

---
+++ variables.js
@@ -0,0 +1,25 @@
+// var, let, const
+// var is globally scoped
+// let and const are block level scoped
+
+// initialization is mandatory while defining constants
+const x = 11;
+// x = 10;
+console.log(x);
+
+// Primitive datatypes
+const fname = 'Suraj';
+const age = 19;
+const CPI = 9.31;
+const isGood = true;
+const a = null;
+const b = undefined;
+let c;  // same as above
+console.log(typeof fname);
+console.log(typeof age);
+console.log(typeof CPI);
+console.log(typeof isGood);
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+
