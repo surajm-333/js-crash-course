7409002dd74bee2413c03c555401053829fa13d9
surajm-333 <surajmulchandani37@gmail.com>
2021-09-13 17:26:55 +0530

    Learnt about strings and its methods.

---
+++ string.js
@@ -0,0 +1,21 @@
+// Concatenation
+
+const fname = 'Suraj';
+const age = 19;
+
+// using +
+console.log('My name is ' + fname + ' and I am ' + age);
+
+// using template strings
+console.log(`My name is ${fname} and I am ${age}`);
+
+let s = 'Hello Duniya!';
+console.log(s.length);
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+console.log(s.substring(2,9).toUpperCase());
+console.log(s.substr(5,4).toUpperCase());
+console.log(s.split(''));
+console.log(s.split('l'));
+console.log(s.split('o'));
+
